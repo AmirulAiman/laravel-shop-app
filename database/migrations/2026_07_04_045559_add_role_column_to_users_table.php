34d10a814957799850ab2ef2489cd8d238926aeb
Amirul Aiman <aiman_amirul@ymail.com>
2026-07-05 17:59:57 +0800

    refactor: modify Users column migration

--- database/migrations/2026_07_04_045559_add_role_column_to_users_table.php
+++ database/migrations/2026_07_04_045559_add_role_column_to_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('customer'); //admin | shop_owner | customer
+            $table->string('role')->after('email')->default('customer'); //admin | shop_owner | customer
         });
     }
 
